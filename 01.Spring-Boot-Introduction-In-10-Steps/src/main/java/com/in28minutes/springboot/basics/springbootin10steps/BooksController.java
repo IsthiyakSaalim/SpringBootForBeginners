e7b2ccfe673084bda8f288b4a811901dbdaa1226
Isthiyak <salim2592@gmail.com>
2022-03-02 11:36:54 +0530

    added a new method to add books

--- 01.Spring-Boot-Introduction-In-10-Steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
+++ 01.Spring-Boot-Introduction-In-10-Steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
@@ -5,10 +5,18 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BooksController {
+	
+	@PostMapping("/books")
+	public Book addBook(@RequestBody Book book) {
+		/*save book to database*/
+		return book;
+	}
 	
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
